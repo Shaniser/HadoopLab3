ed46d2019db689a2ad40023d96ba792026114daf
Aleksey Kostyuchenko <mr.shaniser@gmail.com>
2020-12-21 11:27:57 +0300

    Scripted auto-commit on change (2020-12-21 11:27:57) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/lab3/AirportFindDelay.java
+++ src/main/java/ru/bmstu/hadoop/lab3/AirportFindDelay.java
@@ -5,6 +5,13 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class AirportFindDelay {
+    private static final String FIRST_STRING = "\"YEAR\"";
+    public static final int DELAY_INDEX = 18;
+    public static final int CANCEL_CODE_INDEX = 19;
+    public static final int ID_INDEX = 14;
+    public static final int ERROR_CODE = 1;
+    public static final String DELIMITER = ",";
+    
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
