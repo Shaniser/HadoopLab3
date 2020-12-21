9e9df6dd18dd1c6241ef15fa5567adb3fc4dc17e
Aleksey Kostyuchenko <mr.shaniser@gmail.com>
2020-12-21 13:40:27 +0300

    Scripted auto-commit on change (2020-12-21 13:40:27) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/lab3/FlightInfo.java
+++ src/main/java/ru/bmstu/hadoop/lab3/FlightInfo.java
@@ -1,2 +1,8 @@
-package ru.bmstu.hadoop.lab3;public class FlightInfo {
+package ru.bmstu.hadoop.lab3;
+
+public class FlightInfo {
+    private float maxDelay;
+    private int flightCount;
+    private int cancelledCount;
+    private int delayedCount;
 }
