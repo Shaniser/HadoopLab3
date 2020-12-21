32c6c1150d7eb5b1bd85e4079aee2d773b7eae00
Aleksey Kostyuchenko <mr.shaniser@gmail.com>
2020-12-21 11:16:34 +0300

    Scripted auto-commit on change (2020-12-21 11:16:34) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/lab3/AirportFindDelay.java
+++ src/main/java/ru/bmstu/hadoop/lab3/AirportFindDelay.java
@@ -1,4 +1,10 @@
 package ru.bmstu.hadoop.lab3;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
 public class AirportFindDelay {
+    SparkConf conf = new SparkConf().setAppName("lab3");
+    JavaSparkContext sc = new JavaSparkContext(conf);
+
 }
